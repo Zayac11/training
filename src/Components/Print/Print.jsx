44657a20ce675b4119a58e9f2edff34504a6b16a
Zayac11 <brunogrbstudios@gmail.com>
2021-05-09 13:22:19 +0300

    added small size print ticket

--- src/Components/Print/Print.jsx
+++ src/Components/Print/Print.jsx
@@ -8,21 +8,21 @@ import border from '../../assets/images/border.svg'
 const Print = () => {
 
     const tickets = [
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
-        {title: 'Название выставки'},
+        {title: 'Билет 1'},
+        {title: 'Билет 2'},
+        {title: 'Билет 3'},
+        {title: 'Билет 4'},
+        {title: 'Билет 5'},
+        {title: 'Билет 6'},
+        {title: 'Билет 7'},
+        {title: 'Билет 8'},
+        {title: 'Билет 9'},
+        {title: 'Билет 10'},
+        {title: 'Билет 11'},
     ]
 
     const componentRef = useRef(null);
-    const [size, setSize] = useState('medium')
+    const [size, setSize] = useState('small')
 
     return (
         <div>
@@ -40,12 +40,17 @@ const Print = () => {
                     tickets.map((t, index) => {
                         return (
                             <div className={s.ticketItem} key={index}>
-                                <div>
-                                    {t.title}
-                                </div>
-                                <div className={s.ticketImageContainer}>
-                                    <img className={s.border} src={border} alt="border"/>
-                                    <img className={s.qrcode} src="http://qrcoder.ru/code/?vk.com%2Falexgeniusman&6&0" width="198" height="198" border="0" title="QR код"/>
+                                <div className={s.content}>
+                                    <div className={s.title}>
+                                        {t.title}
+                                    </div>
+                                    <p className={s.description}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    </p>
+                                    <div className={s.ticketImageContainer}>
+                                        <img className={s.border} src={border} alt="border"/>
+                                        <img className={s.qrcode} src="http://qrcoder.ru/code/?vk.com%2Falexgeniusman&6&0" width="198" height="198" border="0" title="QR код"/>
+                                    </div>
                                 </div>
                             </div>
                         )
--- src/Components/Print/TicketItem.jsx
+++ src/Components/Print/TicketItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import cl from 'classnames'
 import './TicketItem.scss'
 import border from "../../assets/images/border.svg";
@@ -6,6 +6,19 @@ let QRCode = require('qrcode.react');
 
 const TicketItem = (props) => {
     let {size, tickets} = props
+    const [sizeValue, setSizeValue] = useState(240)
+
+    useEffect(()=> {
+        if(size === 'medium') {
+            setSizeValue(240)
+        }
+        else if (size === 'medium') {
+            setSizeValue(240)
+        }
+        else {
+            setSizeValue(240)
+        }
+    }, [size])
 
     return (
         <div className={cl('ticketItem', `ticketItem__${size}`)}>
@@ -13,18 +26,19 @@ const TicketItem = (props) => {
                 tickets.map((ticket, index) => {
                     return (
                         <div key={index} className={`ticketItem__ticketLayout ticketItem__ticketLayout__${size}`}>
-                            <div className={`ticketItem__ticketLayout__${size}__title`}>{ticket.title}</div>
-                            <p className={`ticketItem__ticketLayout__${size}__description`}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                            <div className={`ticketItem__ticketLayout__${size}__imgContainer`}>
-                                <img className={`ticketItem__ticketLayout__${size}__border`} src={border} alt="border"/>
-                                <QRCode
-                                    className={`ticketItem__ticketLayout__${size}__image`}
-                                    value={'https://vk.com/alexgeniusman'}
-                                    size={240}
-                                />
-                                {/*<img className={`ticketItem__ticketLayout__${size}__image`} src="http://qrcoder.ru/code/?vk.com%2Falexgeniusman&6&0" width="198" height="198" border="0" title="QR код"/>*/}
+                            <div className={`ticketItem__ticketLayout__${size}__content`}>
+                                <div className={`ticketItem__ticketLayout__${size}__title`}>{ticket.title}</div>
+                                <p className={`ticketItem__ticketLayout__${size}__description`}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <div className={`ticketItem__ticketLayout__${size}__imgContainer`}>
+                                    <img className={`ticketItem__ticketLayout__${size}__border`} src={border} alt="border"/>
+                                    <QRCode
+                                        className={`ticketItem__ticketLayout__${size}__image`}
+                                        value={'https://vk.com/alexgeniusman'}
+                                        size={160}
+                                    />
+                                </div>
                             </div>
                         </div>
                     )
