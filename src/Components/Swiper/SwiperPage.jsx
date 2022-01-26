8f39b6106500d16ce6e312003e5fe3d8d5a6a464
Zayac11 <brunogrbstudios@gmail.com>
2022-01-26 20:44:53 +0300

    swiper cards added

--- src/Components/Swiper/SwiperPage.jsx
+++ src/Components/Swiper/SwiperPage.jsx
@@ -1,8 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import 'swiper/swiper.scss'
+import 'swiper/swiper-bundle.css'
 import s from './Swiper.module.scss'
 import SwiperExample from './SwiperExample/SwiperExample'
+import SwiperCore, {
+    EffectCards
+} from 'swiper';
+
+SwiperCore.use([EffectCards]);
 
 const SwiperPage = () => {
     const [activeIndex, setActiveIndex] = useState(0)
@@ -11,13 +17,16 @@ const SwiperPage = () => {
     }, [activeIndex])
     return (
         <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={(e) => {
-                console.log('slide change', e)
-                setActiveIndex(e.activeIndex)
-            }}
-            onSwiper={(swiper) => console.log(swiper)}
+            effect={'cards'}
+            // spaceBetween={50}
+            // slidesPerView={1}
+            grabCursor={true}
+            className="mySwiper"
+            // onSlideChange={(e) => {
+            //     console.log('slide change', e)
+            //     setActiveIndex(e.activeIndex)
+            // }}
+            // onSwiper={(swiper) => console.log(swiper)}
         >
             <SwiperSlide><SwiperExample number={1}/></SwiperSlide>
             <SwiperSlide><SwiperExample number={2}/></SwiperSlide>
